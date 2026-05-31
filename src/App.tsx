1e48eefb3d6537cadbc0b456ce8d78555d5a3d81
Dhif216 <dhif_mouadh@hotmail.fr>
2026-05-31 21:41:48 +0300

    Fix SPA routing: Use HashRouter and add 404 redirect for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import Catalog from './pages/Catalog'
 import About from './pages/About'
